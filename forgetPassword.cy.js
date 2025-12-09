75c1e920535d3f41c255b544bec3bc65f7c53a27
M Ridhwan K <mrdhwnkml@gmail.com>
2025-12-09 20:28:13 +0700

    forget password script automation

---
+++ forgetPassword.cy.js
@@ -0,0 +1,10 @@
+describe('Login Page', () => {
+    it('should display the login form', () => {
+        cy.visit('/login');
+        cy.get('form#loginForm').should('be.visible');
+        cy.get('input[name="username"]').should('be.visible');
+        cy.get('input[name="password"]').should('be.visible');
+        cy.get('button[type="submit"]').should('be.visible');
+    });
+
+})
